ff63cbe2ebe92f32dc511289e1029d92b5fe1604
Doi Shintaro <c0a2210630@edu.teu.ac.jp>
2023-04-25 15:40:05 +0900

    6問目

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -73,6 +73,9 @@ def main():
             vx *= -1
         if not tate:
             vy *= -1
+        screen.blit(bb_img, bb_rct)
+        if kk_rct.colliderect(bb_rct):
+            return
 
         pg.display.update()
         clock.tick(1000)
